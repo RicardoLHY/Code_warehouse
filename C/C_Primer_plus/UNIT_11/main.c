aa97faf2a2501ae69388a6163010c247871b45cc
KingJoffrey <53467751+RicardoLHY@users.noreply.github.com>
2020-07-08 01:40:31 +0800

    Update main.c

--- C/C_Primer_plus/UNIT_11/main.c
+++ C/C_Primer_plus/UNIT_11/main.c
@@ -2,8 +2,8 @@
 //  main.c
 //  UNIT_11
 //
-//  Created by 李环宇 on 2019/9/2.
-//  Copyright © 2019 Li Huanyu. All rights reserved.
+//  Created by RicardoLHY on 2019/9/2.
+//  Copyright © 2019 RicardoLHY. All rights reserved.
 //
 
 
